307c38d37ed742a025584b51269c47fa32dfe125
Jacob C <jacobc@Zipcoders-MBP.hsd1.de.comcast.net>
2023-04-02 10:39:51 -0400

    returning obj arr - need to make new arr of T

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,5 +1,6 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -8,37 +9,43 @@ import java.util.Arrays;
  */
 public class ArrayUtility<T> {
     private T[] inputArray;
-    public ArrayUtility(T[] inputArray){
+
+    public ArrayUtility(T[] inputArray) {
         this.inputArray = inputArray;
     }
+
     ArrayList<T> arrayList = new ArrayList<>();
-    public void toArrayList(T[] inputArray){
+
+    public void toArrayList(T[] inputArray) {
 
     }
+
     public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
         return 0;
     }
 
     public T getMostCommonFromMerge(T[] arrayToMerge) {
-        return  null;
+        return null;
     }
 
     public Integer getNumberOfOccurrences(T valueToEvaluate) {
         Integer count = 0;
         for (int i = 0; i < inputArray.length; i++) {
-            if (inputArray[i] == valueToEvaluate){
+            if (inputArray[i] == valueToEvaluate) {
                 count += 1;
             }
         }
-        return  count;
+        return count;
     }
 
     public T[] removeValue(T valueToRemove) {
-        this.arrayList.addAll(Arrays.asList(inputArray));
-        for (int i = 0; i < arrayList.size(); i++) {
-            arrayList.remove(i);
+        this.arrayList.addAll(Arrays.asList(inputArray)); //coping inputarray to arraylist to make removal easier
+
+        for (int i = arrayList.size() -1; i > 0; i--) { //iterating down so size of arraylist does not cause issues
+            if (valueToRemove == arrayList.get(i)) {
+                arrayList.remove(i);
+            }
         }
-        T[] whatsLeft = (T[]) arrayList.toArray();
-        return  whatsLeft;
+        return (T[]) arrayList.toArray();
     }
 }
