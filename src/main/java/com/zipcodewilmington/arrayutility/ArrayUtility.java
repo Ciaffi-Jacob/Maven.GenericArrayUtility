fa72366326bf80343a2ed357750fb1d9b4f55349
Jacob C <jacobc@Zipcoders-MBP.hsd1.de.comcast.net>
2023-04-02 09:47:38 -0400

    Generic setup

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,25 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    private T[] inputArray;
+    public ArrayUtility(T[] inputArray){
+        this.inputArray = inputArray;
+    }
+    ArrayList<T> arrayList = new ArrayList<>();
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+    }
+
+    public T[] removeValue(T valueToRemove) {
+    }
 }
